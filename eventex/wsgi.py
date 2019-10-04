fcd82ac37eaf9d457a26c0eca7e7d8b123e080bf
tmassis <thiago3np@gmai.com>
2019-10-04 00:09:21 -0300

    Initial commit com wsgi

--- eventex/wsgi.py
+++ eventex/wsgi.py
@@ -11,6 +11,6 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventex.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventex.settings")
 
 application = Cling(get_wsgi_application())
